a6c0844c32763f88c4472096bbf3c256222072c4
MuradKil <162470338+MuradKil@users.noreply.github.com>
2024-09-22 12:07:51 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -130,13 +130,12 @@ local GetSword = Tabs.Player:AddButton({
     Title = "Get Sword",
     Description = "Add Sword to your Backpack",
     Callback = function()
-       local args = {
-    [1] = "Weapons",
-    [2] = "Sword"
-}
+        local args = {
+            [1] = "Weapons",
+            [2] = "Sword"
+        }
 
-game:GetService("ReplicatedStorage").Remotes.Shop.EquipItem:InvokeServer(unpack(args))
-
+        game:GetService("ReplicatedStorage").Remotes.Shop.EquipItem:InvokeServer(unpack(args))
     end
 })
 
